7b6fee7618877f78d355ff1521c0b08904971bf5
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-06-13 00:31:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+  this.age = age;
+}
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	 Person.call(this, name, age); // Call the Person constructor with the current instance
+  this.jobTitle = jobTitle;
+}
+
+// Inheriting the Person prototype in the Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+// Adding jobGreet method to Employee prototype
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+}
 
 // Do not change code below this line
 window.Person = Person;
